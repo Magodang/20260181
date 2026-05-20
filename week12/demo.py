0517b01855fbbf99208b5a02860dafe24880fdb1
Magodang <jin49782@gmail.com>
2026-05-20 17:38:33 +0900

    5.20

--- week12/demo.py
+++ week12/demo.py
@@ -1,5 +1,74 @@
 import pygame
 import sys
+import os
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+class SpriteSheet:
+
+    def __init__(self, path):
+
+        self.sheet = pygame.image.load(
+            resource_path(path)
+        ).convert_alpha()
+
+    def get_image(self, x, y, width, height):
+
+        image = pygame.Surface(
+            (width, height),
+            pygame.SRCALPHA
+        )
+
+        image.blit(
+            self.sheet,
+            (0, 0),
+            (x, y, width, height)
+        )
+
+        return image
+    
+
+def load_animation(
+    sheet,
+    frame_width,
+    frame_height,
+    frame_count,
+    scale=1,
+    row=0
+):
+
+    frames = []
+
+    for i in range(frame_count):
+
+        frame = sheet.get_image(
+
+            i * frame_width,
+            row * frame_height,
+
+            frame_width,
+            frame_height
+        )
+
+        scaled_width = int(frame_width * scale)
+        scaled_height = int(frame_height * scale)
+
+        frame = pygame.transform.scale(
+            frame,
+            (scaled_width, scaled_height)
+        )
+
+        frames.append(frame)
+
+    return frames
+
 
 pygame.init()
 
@@ -22,6 +91,18 @@ WORLD_WIDTH = 4000
 
 ground = pygame.Rect(0, HEIGHT - 80, WORLD_WIDTH, 80)
 
+enemy1_sheet = SpriteSheet("week12/assets/en1.png")
+
+enemy1_animations = {
+    "idle": load_animation(
+        enemy1_sheet,
+        245,
+        220,
+        2,
+        scale = 0.5
+    )
+}
+
 # 플랫폼
 platforms = [
     pygame.Rect(300, 560, 200, 20),
@@ -43,9 +124,14 @@ walls = [
 
 # 적
 enemies = [
-    pygame.Rect(750, HEIGHT - 140, 40, 60),
-    pygame.Rect(1700, HEIGHT - 140, 40, 60),
-    pygame.Rect(2800, HEIGHT - 140, 40, 60),
+
+    {
+        "rect": pygame.Rect(750, HEIGHT - 300, 125, 110),
+        "animations": enemy1_animations,
+        "current_animation": "idle",
+        "frame_index": 0,
+        "animation_speed": 0.05
+    }
 ]
 
 player = pygame.Rect(100, HEIGHT - 200, 40, 60)
@@ -57,12 +143,14 @@ jump_power = -16
 gravity = 0.8
 on_ground = False
 extra_jumps = 1
-coyote_time = 10
+coyote_time = 6
 coyote_timer = 0
-jump_buffer_time = 10
-jump_buffer_timer = 0
+input_buffer = None
+input_buffer_time = 6
+input_buffer_timer = 0
 
 camera_x = 0
+camera_y = 0
 
 # =========================
 # 게임 루프
@@ -72,6 +160,17 @@ running = True
 while running:
 
     dt = clock.tick(60)
+
+    # =========================
+    # 적 애니
+    # =========================
+    for enemy in enemies:
+        enemy["frame_index"] += enemy["animation_speed"]
+        animation = enemy["animations"][
+            enemy["current_animation"]
+        ]
+        if enemy["frame_index"] >= len(animation):
+            enemy["frame_index"] = 0
 
     # =====================
     # 이벤트 처리
@@ -91,7 +190,8 @@ while running:
             ):
 
                 # 점프 입력 저장
-                jump_buffer_timer = jump_buffer_time
+                input_buffer = "jump"
+                input_buffer_timer = input_buffer_time
 
     # =====================
     # 입력 처리
@@ -143,8 +243,10 @@ while running:
     # =====================
     # 점프 버퍼 감소
     # =====================
-    if jump_buffer_timer > 0:
-        jump_buffer_timer -= 1
+    if input_buffer_timer > 0:
+        input_buffer_timer -= 1
+    else:
+        input_buffer = None
 
     # =====================
     # 코요테 감소
@@ -155,25 +257,21 @@ while running:
     # =====================
     # 점프 실행
     # =====================
-    if jump_buffer_timer > 0:
+    if input_buffer == "jump":
 
         # 일반 점프
         if was_on_ground or coyote_timer > 0:
 
             vel_y = jump_power
-
+            on_ground = False
             coyote_timer = 0
-
-            jump_buffer_timer = 0
+            input_buffer_timer = 0
 
         # 공중 점프
         elif extra_jumps > 0:
-
             vel_y = jump_power
-
             extra_jumps -= 1
-
-            jump_buffer_timer = 0
+            input_buffer = None
 
     # =====================
     # 중력
@@ -235,13 +333,20 @@ while running:
     # =====================
     # 카메라
     # =====================
-    camera_x = player.centerx - WIDTH // 2
+    target_x = player.centerx - WIDTH // 2
+    target_y = player.centery - HEIGHT // 2
+
+    camera_x += (target_x - camera_x) * 0.1
+    camera_y += (target_y - camera_y) * 0.1
 
     if camera_x < 0:
-        camera_x = 0
-
+        camera_x =0
+    
     if camera_x > WORLD_WIDTH - WIDTH:
         camera_x = WORLD_WIDTH - WIDTH
+
+    if camera_y < 0:
+        camera_y = 0
 
     # =====================
     # 렌더링
@@ -254,7 +359,7 @@ while running:
         GROUND_COLOR,
         (
             ground.x - camera_x,
-            ground.y,
+            ground.y - camera_y,
             ground.width,
             ground.height
         )
@@ -268,7 +373,7 @@ while running:
             PLATFORM_COLOR,
             (
                 platform.x - camera_x,
-                platform.y,
+                platform.y - camera_y,
                 platform.width,
                 platform.height
             )
@@ -282,7 +387,7 @@ while running:
             WALL_COLOR,
             (
                 wall.x - camera_x,
-                wall.y,
+                wall.y - camera_y,
                 wall.width,
                 wall.height
             )
@@ -291,14 +396,19 @@ while running:
     # 적
     for enemy in enemies:
 
-        pygame.draw.rect(
-            screen,
-            ENEMY_COLOR,
+        animation = enemy["animations"][
+            enemy["current_animation"]
+        ]
+
+        image = animation[
+            int(enemy["frame_index"])
+        ]
+
+        screen.blit(
+            image,
             (
-                enemy.x - camera_x,
-                enemy.y,
-                enemy.width,
-                enemy.height
+                int(enemy["rect"].x - camera_x),
+                int(enemy["rect"].y - camera_y)
             )
         )
 
@@ -308,7 +418,7 @@ while running:
         PLAYER_COLOR,
         (
             player.x - camera_x,
-            player.y,
+            player.y - camera_y,
             player.width,
             player.height
         )
@@ -316,7 +426,7 @@ while running:
 
     shadow_rect = pygame.Rect(
         player.x - camera_x + 5,
-        player.bottom - 8,
+        player.bottom - camera_y - 8,
         player.width - 10,
         8
     )
